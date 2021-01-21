1a86f2c9c8c36d3d8638a42a47bd397c2c41df90
Daniel DeGrasse <daniel@degrasse.com>
2021-01-21 01:38:32 -0600

    Filled out majority of sensor service method stubs

--- src/app/services/sensor/sensor.service.ts
+++ src/app/services/sensor/sensor.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { Sensor } from '../../models/sensor';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Configuration } from '../../../config'
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,63 +12,69 @@ export class SensorService {
 
   constructor(private http: HttpClient) { }
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+
   /**
    * Gets all sensors from the backend
    */
   getSensors(): Observable<Sensor[]> {
-    return this.http.get(`${Configuration.api_url}/sensors`).pipe(map(res => res as Sensor[]))
+    return this.http.get(`${Configuration.api_url}/sensors`).pipe(catchError(this.handleError)).pipe(map(res => res as Sensor[]))
   }
 
   /**
    * Creates a new sensor with a POST request
    * @param sensor: Sensor to create
    */
-  createSensor(sensor: Sensor): Promise<Sensor> {
+  createSensor(sensor: Sensor): Observable<Sensor> {
     // Use spread syntax
-    return new Promise<Sensor>((resolve, reject) => {
-      // Validate parameters before sending to backend.
-      if (Math.abs(sensor.location.latitude) > 90 || Math.abs(sensor.location.longitude) > 180) {
-        return reject("Latitude or longitude out of range");
-      }
-      return this.http.post(`${Configuration.backend_url}/sensors`,
-        {
-          hardware_id: sensor.hardware_id,
-          install_date: sensor.install_date.toISOString(),
-          location: {
-            latitude: sensor.location.latitude,
-            longitude: sensor.location.longitude
-          }
-        }).toPromise();
-    });
+    return this.http.post(`${Configuration.backend_url}/sensors`,
+      {
+        hardware_id: sensor.hardware_id,
+        install_date: sensor.install_date.toISOString(),
+        location: {
+          latitude: sensor.location.latitude,
+          longitude: sensor.location.longitude
+        }
+      }).pipe(catchError(this.handleError)).pipe(map(res => res as Sensor));
   }
 
   /**
    * Gets a single sensor
-   * @param synthetic_id Sythentic id of sensor to get
+   * @param synthetic_id Synthetic id of sensor to get
    */
-  getSensor(synthetic_id: number): Promise<Sensor> {
-    return new Promise((resolve, reject) => {
-      reject("Not yet implemented");
-    });
+  getSensor(synthetic_id: number): Observable<Sensor> {
+    return this.http.get(`${Configuration.backend_url}/sensors/${synthetic_id}`)
+      .pipe(catchError(this.handleError)).pipe(map(res => res as Sensor))
   }
 
   /**
    * Deletes a sensor from the backend
    * @param sensor Sensor to delete
    */
-  deleteSensor(sensor: Sensor): Promise<void> {
-    return new Promise((resolve, reject) => {
-      reject("Not yet implemented");
-    });
+  deleteSensor(sensor: Sensor): Observable<void> {
+    return this.http.delete(`${Configuration.backend_url}/sensors/${sensor.synthetic_id}`)
+    .pipe(catchError(this.handleError)).pipe(map(res => {}));
   }
 
   /**
    * Replaces a sensor on the backend with a PUT request
    * @param: newSensor: New Sensor to replace old one
    */
-  updateSensor(newSensor: Sensor): Promise<Sensor> {
-    return new Promise((resolve, reject) => {
-      reject("Not yet implemented");
-    });
+  updateSensor(newSensor: Sensor): Observable<Sensor> {
+    return throwError("Not yet implemented");
   }
 }
