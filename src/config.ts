2f1c52ecdfc01b8314975754ec187127d87990ab
Daniel DeGrasse <daniel@degrasse.com>
2021-02-04 16:07:49 -0600

    Added configuration for production

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,12 @@
 /**
  * Configuration data for the backend.
  */
+
+// Import environment
+import { environment } from './environments/environment';
+
 export class Configuration {
-    public static backend_url = 'http://localhost:8000';
+    public static backend_url = environment.production ? 'http://3.21.41.182' : 'http://localhost:8000';
     public static api_url = `${Configuration.backend_url}/api`;
     public static auth_url = `${Configuration.backend_url}/authentication`;
 }
